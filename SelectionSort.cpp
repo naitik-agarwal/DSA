2e6dfc09ad00e08da089e2d134d36fc56e01fd46
naitik-agarwal <a.naitik@iitg.ac.in>
2025-09-27 22:52:02 +0530

    few comments added

--- SelectionSort.cpp
+++ SelectionSort.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 void SelectionSort(vector<int> &a){  // thats pass by reference
-    for(int i=0;i<a.size()-1;i++){
+    for(int i=0;i<a.size()-1;i++){ // total n-1 times
         int r=i;
         for(int j=i+1;j<a.size();j++){
             if(a[j]<a[r]){
